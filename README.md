4b8f2f3471eab088e9fdecf25935395cc1dfc8dc
air-hand <45233435+air-hand@users.noreply.github.com>
2022-10-17 22:42:46 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Environments
+
+Docker provider for Terraform
+
+Terraform CLI in docker container (DooD)
+
+# How to use?
+
+make
+
+terraform plan
+
+terraform apply
---
+++ src/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
+
+provider "docker" {
+  # DooD
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.name
+  name  = "tf-tutorial"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
